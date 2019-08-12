b4129ad8397361341dcd29c36c583196a96959a3
Vorrawut Judasri <vorrawutjud@gmail.com>
2019-08-12 21:12:43 +0700

    add logic to Auto Scroll from view when focus input text

--- KeyboardAutoScrollView/ViewController.swift
+++ KeyboardAutoScrollView/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    // View to focus when show keyboard
+    private var activeField: UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +21,44 @@ class ViewController: UIViewController {
         //Looks for single or multiple taps.
         let tapper = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
         view.addGestureRecognizer(tapper)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWasShown(notification: Notification) {
+        let info = notification.userInfo! as NSDictionary
+        let keyboardInfo = info[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        
+        // use UIKeyboardFrameEndUserInfoKey to fix iOS11 keyboard height
+        var keyboardSize = keyboardInfo.cgRectValue.size
+        
+        // Add safeAreaInsets.bottom offset for iPhoneX
+        if #available(iOS 11.0, *) {
+            keyboardSize.height += view.safeAreaInsets.bottom
+        }
+        
+        // Add bottom content inset [clear when hide keyboard event fired (keyboardWillHide:)]
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        
+        var aRect = view.frame
+        aRect.size.height -= keyboardSize.height
+        
+        if let activeField = activeField {
+            //convert frame into scrollView coordinate
+            let convertedFrame = activeField.convert(activeField.bounds, to: scrollView)
+            if !aRect.contains(convertedFrame.origin) {
+                scrollView.scrollRectToVisible(convertedFrame, animated: true)
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        // Reset bottom content inset
+        scrollView.contentInset = .zero
+        scrollView.scrollIndicatorInsets = .zero
     }
     
     @objc func dismissKeyboard() {
